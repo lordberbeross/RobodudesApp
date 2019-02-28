db3ceaf0cfd8e74dccde8ad947be744e05cb4ea3
LordBerbeross <mustafaberber020@gmail.com>
2019-02-28 22:30:47 +0300

    Updates

---
+++ precache-manifest.b9889ffb163675d3c745687dbd141ab9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/lordberbeross/RobodudesApp.git/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8412e1d478257866c2f3",
+    "url": "/lordberbeross/RobodudesApp.git/static/js/runtime~main.8412e1d4.js"
+  },
+  {
+    "revision": "4a0fede06f76d21ebd9a",
+    "url": "/lordberbeross/RobodudesApp.git/static/js/main.4a0fede0.chunk.js"
+  },
+  {
+    "revision": "8b17ced68c2b6c4c2c99",
+    "url": "/lordberbeross/RobodudesApp.git/static/js/2.8b17ced6.chunk.js"
+  },
+  {
+    "revision": "4a0fede06f76d21ebd9a",
+    "url": "/lordberbeross/RobodudesApp.git/static/css/main.c90285a1.chunk.css"
+  },
+  {
+    "revision": "8b17ced68c2b6c4c2c99",
+    "url": "/lordberbeross/RobodudesApp.git/static/css/2.a154ec9f.chunk.css"
+  },
+  {
+    "revision": "870ccf82ac8d989fc3e55e90165f712e",
+    "url": "/lordberbeross/RobodudesApp.git/index.html"
+  }
+];
